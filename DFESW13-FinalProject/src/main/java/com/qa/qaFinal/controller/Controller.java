22f1df6b204a667377051aab3239adae5f7ea5b4
Dan <danielhibbard1234@gmail.com>
2022-03-21 12:41:23 +0000

    DH: Update by id and delete by id completed

--- DFESW13-FinalProject/src/main/java/com/qa/qaFinal/controller/Controller.java
+++ DFESW13-FinalProject/src/main/java/com/qa/qaFinal/controller/Controller.java
@@ -4,13 +4,18 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.qaFinal.model.Pokedex;
 import com.qa.qaFinal.services.Services;
+
+
 
 
 
@@ -22,6 +27,8 @@ public class Controller {
 	public Controller(Services service) {
 		super();
 		this.service = service;
+		
+		
 	}
 	
 	@PostMapping("/createEntry")
@@ -43,4 +50,36 @@ public class Controller {
 	   return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
 
 }
+	
+	@GetMapping("/getId/{id}")
+	public ResponseEntity<Pokedex> getById(@PathVariable("id") long id) {
+		
+		
+		Pokedex result = service.getById(id);
+		
+		
+		ResponseEntity<Pokedex> response = new ResponseEntity<>(result, HttpStatus.ACCEPTED);
+		
+		return response;
+	}
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<String> updateByid(@PathVariable("id") long id, @RequestBody Pokedex pokedex) {
+		
+		service.update(id, pokedex); // Telling the Service to do the method, but not doing anything with the return
+		
+		String response = "Updating pokemon of id: " + id;
+		return new ResponseEntity<>(response, HttpStatus.OK);
+   
 }
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> deleteByid(@PathVariable("id") long id) {
+		service.remove(id);
+		String response = "Pokemon of id: " + id + " deleted";
+		return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+	}
+	
+	
+}	
+	
--- DFESW13-FinalProject/src/main/java/com/qa/qaFinal/services/Services.java
+++ DFESW13-FinalProject/src/main/java/com/qa/qaFinal/services/Services.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import com.qa.qaFinal.model.Pokedex;
 import com.qa.qaFinal.repo.Repo;
 
+
 @Service
 public class Services {
 	
@@ -24,6 +25,38 @@ public class Services {
 	
 	public List<Pokedex> getEntries() {
 		return repo.findAll();
+	}
+
+	public Pokedex getById(long id) {
+		
+		return repo.findById(id).get();
+	}
+	
+	
+	
+	
+	public boolean update(long id, Pokedex pokedex) {
+		
+		
+		Pokedex oldEntry = getById(id);
+		oldEntry.setName(pokedex.getName());
+		oldEntry.setType(pokedex.getType());
+		oldEntry.setHp(pokedex.getHp());
+		oldEntry.setDescription(pokedex.getDescription());
+	
+		Pokedex updatedPokemon = oldEntry;
+		
+		repo.save(updatedPokemon);
+		return true;
+		
+		
+		
+	}
+
+	public boolean remove(long id) {
+		repo.deleteById(id);
+		return true;
+		
 	}
 
 }
